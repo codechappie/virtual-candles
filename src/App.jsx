48a22e1ce0871029e64f54334915cf7b0f82c5f0
CodeChappie <acanalesa14@gmail.com>
2024-02-13 23:56:08 -0500

    upload main structure

--- src/App.jsx
+++ src/App.jsx
@@ -62,17 +62,17 @@ function App() {
             // Verificar si se supera el umbral para detener la escucha
             const positiveDb = (dB > 0 ? dB : dB * -1).toFixed(0);
 
-            if (positiveDb < 50) {
+            if (positiveDb < 121) {
               decibelMeter.textContent = 'NAADDA', positiveDb;
               setAction("nada")
             }
 
-            if (positiveDb < 40 && positiveDb > 30) {
+            if (positiveDb < 120 && positiveDb > 50) {
               decibelMeter.textContent = 'Soplando...', positiveDb;
               setAction("soplando")
             }
 
-            if (positiveDb < 29) {
+            if (positiveDb < 49) {
               decibelMeter.textContent = 'APAGADO', positiveDb;
               setAction("apagado");
 
